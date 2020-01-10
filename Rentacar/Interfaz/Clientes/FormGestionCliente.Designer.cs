0857ec1a363a023c8213f6f6167fe9bedaae3f53
optimarentacar <optimarentacar2019@gmail.com>
2020-01-10 21:40:16 +0100

    Interfaz Gestion Alumnos Terminada

--- Rentacar/Interfaz/Clientes/FormGestionCliente.Designer.cs
+++ Rentacar/Interfaz/Clientes/FormGestionCliente.Designer.cs
@@ -46,6 +46,13 @@
             this.ValidarDni = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.Primero = new System.Windows.Forms.Button();
+            this.Atras = new System.Windows.Forms.Button();
+            this.Siguiente = new System.Windows.Forms.Button();
+            this.Ultimo = new System.Windows.Forms.Button();
+            this.Añadir = new System.Windows.Forms.Button();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.Modificar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -137,6 +144,7 @@
             // 
             // textDni
             // 
+            this.textDni.Enabled = false;
             this.textDni.Location = new System.Drawing.Point(13, 395);
             this.textDni.Name = "textDni";
             this.textDni.Size = new System.Drawing.Size(139, 20);
@@ -144,6 +152,7 @@
             // 
             // textTelefono
             // 
+            this.textTelefono.Enabled = false;
             this.textTelefono.Location = new System.Drawing.Point(12, 448);
             this.textTelefono.Name = "textTelefono";
             this.textTelefono.Size = new System.Drawing.Size(139, 20);
@@ -151,6 +160,7 @@
             // 
             // textNombreApellido
             // 
+            this.textNombreApellido.Enabled = false;
             this.textNombreApellido.Location = new System.Drawing.Point(212, 395);
             this.textNombreApellido.Name = "textNombreApellido";
             this.textNombreApellido.Size = new System.Drawing.Size(261, 20);
@@ -158,6 +168,7 @@
             // 
             // textDomicilio
             // 
+            this.textDomicilio.Enabled = false;
             this.textDomicilio.Location = new System.Drawing.Point(212, 448);
             this.textDomicilio.Name = "textDomicilio";
             this.textDomicilio.Size = new System.Drawing.Size(261, 20);
@@ -198,11 +209,88 @@
             this.button2.TabIndex = 12;
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // Primero
+            // 
+            this.Primero.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Primero.BackgroundImage")));
+            this.Primero.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Primero.Location = new System.Drawing.Point(30, 490);
+            this.Primero.Name = "Primero";
+            this.Primero.Size = new System.Drawing.Size(80, 35);
+            this.Primero.TabIndex = 13;
+            this.Primero.UseVisualStyleBackColor = true;
+            // 
+            // Atras
+            // 
+            this.Atras.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Atras.BackgroundImage")));
+            this.Atras.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Atras.Location = new System.Drawing.Point(116, 490);
+            this.Atras.Name = "Atras";
+            this.Atras.Size = new System.Drawing.Size(80, 35);
+            this.Atras.TabIndex = 14;
+            this.Atras.UseVisualStyleBackColor = true;
+            // 
+            // Siguiente
+            // 
+            this.Siguiente.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Siguiente.BackgroundImage")));
+            this.Siguiente.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Siguiente.Location = new System.Drawing.Point(202, 490);
+            this.Siguiente.Name = "Siguiente";
+            this.Siguiente.Size = new System.Drawing.Size(80, 35);
+            this.Siguiente.TabIndex = 15;
+            this.Siguiente.UseVisualStyleBackColor = true;
+            // 
+            // Ultimo
+            // 
+            this.Ultimo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Ultimo.BackgroundImage")));
+            this.Ultimo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Ultimo.Location = new System.Drawing.Point(288, 490);
+            this.Ultimo.Name = "Ultimo";
+            this.Ultimo.Size = new System.Drawing.Size(80, 35);
+            this.Ultimo.TabIndex = 16;
+            this.Ultimo.UseVisualStyleBackColor = true;
+            // 
+            // Añadir
+            // 
+            this.Añadir.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Añadir.BackgroundImage")));
+            this.Añadir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Añadir.Location = new System.Drawing.Point(374, 490);
+            this.Añadir.Name = "Añadir";
+            this.Añadir.Size = new System.Drawing.Size(80, 35);
+            this.Añadir.TabIndex = 17;
+            this.Añadir.UseVisualStyleBackColor = true;
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Eliminar.BackgroundImage")));
+            this.Eliminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Eliminar.Location = new System.Drawing.Point(460, 490);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(80, 35);
+            this.Eliminar.TabIndex = 18;
+            this.Eliminar.UseVisualStyleBackColor = true;
+            // 
+            // Modificar
+            // 
+            this.Modificar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Modificar.BackgroundImage")));
+            this.Modificar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Modificar.Location = new System.Drawing.Point(546, 490);
+            this.Modificar.Name = "Modificar";
+            this.Modificar.Size = new System.Drawing.Size(80, 35);
+            this.Modificar.TabIndex = 19;
+            this.Modificar.UseVisualStyleBackColor = true;
+            // 
             // FormGestionCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(654, 537);
+            this.Controls.Add(this.Modificar);
+            this.Controls.Add(this.Eliminar);
+            this.Controls.Add(this.Añadir);
+            this.Controls.Add(this.Ultimo);
+            this.Controls.Add(this.Siguiente);
+            this.Controls.Add(this.Atras);
+            this.Controls.Add(this.Primero);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.ValidarDni);
@@ -219,6 +307,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormGestionCliente";
             this.Text = "Alquiler de Vehículos 1.0";
+            this.Load += new System.EventHandler(this.FormGestionCliente_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -245,5 +334,12 @@
         private System.Windows.Forms.CheckBox ValidarDni;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Primero;
+        private System.Windows.Forms.Button Atras;
+        private System.Windows.Forms.Button Siguiente;
+        private System.Windows.Forms.Button Ultimo;
+        private System.Windows.Forms.Button Añadir;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.Button Modificar;
     }
 }
